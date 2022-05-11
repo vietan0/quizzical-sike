bb186f81cdb59cc0ef45a02d7abb54457c2c1905
Việt An <vietan00892b@gmail.com>
2022-05-11 20:25:10 +0700

    'choices' now hold objects, added choice shuffling

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -7,39 +7,39 @@ export default function Quiz(props) {
 		props.correct_answer,
 		...props.incorrect_answers,
 	]);
-	
-	let [choices, setChoices] = React.useState(() => {
-		let correct = [true, false, false, false];
-		// combine two 1D arrays into a 2D array
-		let order = choiceTexts.map((text, i) => ({
+	let correct = [true, false, false, false];
+
+	// combine two 1D arrays into a 2D array
+	let [choices, setChoices] = React.useState(() =>
+		choiceTexts.map((text, i) => ({
 			text: text,
 			correct: correct[i],
 			selected: false,
-		}));
+		}))
+	);
 
-		let randomed = [];
+	let [choiceDivs, setChoiceDivs] = React.useState(() => {
+		let divs = choices.map(c => (
+			<Choice text={c.text} correct={c.correct} selected={c.selected} key={nanoid()} />
+		));
 		for (let i = 1; i <= 3; i++) {
-			// shuffle the choices
+			// shuffle the divs
 			let randomIndex = Math.floor(Math.random() * 3);
-			randomed = [
-				...order.slice(0, randomIndex),
-				...order.slice(randomIndex + 1),
-				order[randomIndex],
+			divs = [
+				divs[randomIndex],
+				...divs.slice(0, randomIndex),
+				...divs.slice(randomIndex + 1),
 			];
 		}
-		return randomed;
+		return divs;
 	});
 
-	let [choiceDivs, setChoiceDivs] = React.useState(() =>
-		choices.map(c => (
-			<Choice text={c.text} correct={c.correct} selected={c.selected} key={nanoid()} />
-		))
-	);
 	return (
 		<article className="quiz">
+			<pre>{JSON.stringify(props, null, 4)}</pre>
 			{/* <pre>{JSON.stringify(choices, null, 4)}</pre> */}
 
-			<h2>{props.question}</h2> 
+			<h2>{props.question}</h2>
 			<div className="options">{choiceDivs}</div>
 			<hr />
 		</article>
