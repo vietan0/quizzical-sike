0654ebeb3d20e0c4c4fd97299baf7240febee0e4
Việt An <vietan00892b@gmail.com>
2022-05-11 16:13:23 +0700

    Remove Karla typeface

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 
 export default function App() {
-	let numDivs = new Array(50).fill(0).map((_, i) => <div>Item {i + 1}</div>);
+	let numDivs = new Array(15).fill(0).map((_, i) => <div>Item {i + 1}</div>);
 	return (
 		<>
 			<h1>App Banana goes here</h1>
 			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores temporibus dolores
-				atque sint quaerat dolore natus tempore et, voluptatibus, nobis fugit. Earum nulla
-				alias quam et tempora? Inventore ut deleniti quod. Delectus quam praesentium
-				laboriosam, perspiciatis rerum, hic nam consequuntur voluptatem similique vel eum
-				dolor sunt neque et magni maiores?
+				atque sint quaerat dolore natus tempore et, voluptatibus, nobis fugit. Inventore ut
+				deleniti quod. Delectus quam praesentium laboriosam, perspiciatis rerum, hic nam
+				consequuntur voluptatem similique vel eum dolor sunt neque et magni maiores?
 			</p>
-			<button>Send bananas</button>
 			<div className="flex-container">{numDivs}</div>
 		</>
 	);
